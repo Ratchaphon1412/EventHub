d51fc73a85ba343d1cfe9d5623c45dd155806e1d
ratchaphon1412 <ratchaphon.h111@gmail.com>
2023-08-01 11:25:28 +0700

    edit migration file user add more field

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -17,6 +17,14 @@ return new class extends Migration
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
+            $table->string('first_name', 100)->nullable()->comment('First name');
+            $table->string('last_name', 100)->nullable()->comment('Last name');
+            $table->string('phone', 20)->nullable()->comment('Phone number');
+            $table->enum('gender',['Female','Male','Other'])->default('Other');
+            $table->integer('age')->nullable()->comment('Age');
+            $table->string('facebook', 255)->nullable()->comment('facebook');
+            $table->string('instagram', 255)->nullable()->comment('instagram');
+            $table->string('line', 255)->nullable()->comment('line id');
             $table->rememberToken();
             $table->foreignId('current_team_id')->nullable();
             $table->string('profile_photo_path', 2048)->nullable();
