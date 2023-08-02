83353b4a9eea8d503305857cf66b5d98c5d8dfce
ratchaphon1412 <ratchaphon.h111@gmail.com>
2023-08-02 21:34:58 +0700

    install faker

--- resources/views/components/card-background-image.blade.php
+++
@@ -1,8 +0,0 @@
-<div class="card w-96 bg-base-100 shadow-xl image-full">
-    <figure><img src="{{$image}}" alt="Shoes" /></figure>
-    <div class="card-body text-center self-center">
-      <h2 >{{$title}}</h2>
-      <p>{{$description}}</p>
-    
-    </div>
-</div>
