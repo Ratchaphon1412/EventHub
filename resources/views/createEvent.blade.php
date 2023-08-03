00de1af633c49fd569450dc6000ba8596b87f178
Tirawat1 <Tirawat.po@ku.ac.th>
2023-08-03 18:53:30 +0700

    createEventPage

---
+++ resources/views/createEvent.blade.php
@@ -0,0 +1,20 @@
+<head>
+<x-app-layout>
+</head>
+    <main class="flex-col justify-center items-center ">
+        
+        <section id="coverImage" class="relative h-auto w-auto overflow-hidden bg-cover bg-no-repeat">
+            <div class="flex justify-center items-center bg-black h-64 opacity-80 w-full rounded-lg">
+                <button>
+                    <img src="https://png.pngtree.com/element_our/sm/20180516/sm_5afbe35ff3ec9.jpg" alt="Avatar"
+                    class="w-16 h-16 rounded-lg mr-2 bg-black object-cover">
+                </button>
+        </section>
+        <section id="Title" class="flex flex-col w-5/6 justify-center items-center">
+            <label for="" class="pb-1 mt-10">Class</label>
+            <input type="text" class="rounded-lg">
+        </section>
+    </main>
+
+    
+</x-app-layout>
