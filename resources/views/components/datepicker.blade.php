e7ad329f99ee4f45617ec7a2bbd2c160dfb81281
ratchaphon1412 <ratchaphon.h111@gmail.com>
2023-08-08 00:35:44 +0700

    add component datepicker

--- resources/views/components/datepicker.blade.php
+++ resources/views/components/datepicker.blade.php
@@ -7,14 +7,6 @@
              <input type="date" name="date" id="date"
                      class="w-full rounded-md border border-[#e0e0e0] bg-white py-3 px-6 text-base font-medium text-[#6B7280] outline-none focus:border-[#6A64F1] focus:shadow-md" />
             </div>
-            </div>
-                <div class="w-full px-3 sm:w-1/2">
-                    <div class="mb-5">
-                <label for="time" class="mb-3 block text-base font-medium text-[#07074D]">
-                    <br>
-                </label>
-            <input type="time" name="time" id="time"
-            class="w-full rounded-md border border-[#e0e0e0] bg-white py-3 px-6 text-base font-medium text-[#6B7280] outline-none focus:border-[#6A64F1] focus:shadow-md" />
         </div>
     </div>
 </div>
