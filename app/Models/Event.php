01f46a360869dadf9867c4acb55b36050a09e2c9
ratchaphon1412 <ratchaphon.h111@gmail.com>
2023-08-07 21:12:32 +0700

    create database event

---
+++ app/Models/Event.php
@@ -0,0 +1,47 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\HasOne;
+use Illuminate\Database\Eloquent\Relations\HasMany;
+use Illuminate\Database\Eloquent\SoftDeletes;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+
+class Event extends Model
+{
+    use HasFactory;
+
+    public function category():BelongsTo
+    {
+        return $this->belongsTo(Category::class);
+    }
+    
+    public function eventImage():HasMany
+    {
+        return $this->hasMany(EventImage::class);
+    }
+
+    public function user():BelongsTo
+    {
+
+        return $this->belongsTo(User::class);
+    }
+
+    public function team():HasMany
+    {
+        return $this->hasMany(Team::class);
+    }
+
+    public function kanban():HasOne
+    {
+        return $this->hasOne(Kanban::class);
+    }
+
+    
+
+
+    
+
+}
---
+++ database/migrations/2023_08_07_101010_create_events_table.php
@@ -0,0 +1,52 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+use App\Models\Category;
+use App\Models\EventImage;
+use App\Models\User;
+use App\Models\Team;
+use App\Models\Kanban;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('events', function (Blueprint $table) {
+            $table->id();
+            $table->string('title');
+            $table->string('description');
+            $table->foreignIdFor(Category::class);
+            $table->string("image_poster");
+            $table->foreignIdFor(EventImage::class)->nullable();
+            $table->dateTime('date_post');
+            $table->dateTime('registration_start_date');
+            $table->dateTime('registration_end_date');
+            $table->dateTime("announcement_date")->nullable();
+            $table->dateTime("event_start_date");
+            $table->dateTime("event_end_date");
+            $table->string("event_latitude")->nullable();
+            $table->string("event_longitude")->nullable();
+            $table->string("document_payment")->nullable();
+            $table->foreignIdFor(User::class);
+            
+            
+            
+            
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('events');
+    }
+};
