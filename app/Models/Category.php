436832995211ea27a452891f0ea1f4e76cdbfe30
ratchaphon1412 <ratchaphon.h111@gmail.com>
2023-08-07 21:08:34 +0700

    Models Category add Relation ship

--- app/Models/Category.php
+++ app/Models/Category.php
@@ -5,7 +5,17 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\SoftDeletes;
+use Illuminate\Database\Eloquent\Relations\HasMany;
+
 class Category extends Model
 {
     use HasFactory,SoftDeletes;
+
+    
+    public function event():HasMany
+    {
+        return $this->hasMany(Event::class);
+    }
+
+
 }
