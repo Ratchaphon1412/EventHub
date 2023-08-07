53b767482cd91e2d03f05b34bac20d7f790b6f55
ratchaphon1412 <ratchaphon.h111@gmail.com>
2023-08-07 21:10:59 +0700

    edit migrations kanban table

--- database/migrations/2023_08_03_175926_create_kanbans_table.php
+++ database/migrations/2023_08_03_175926_create_kanbans_table.php
@@ -4,6 +4,8 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
+use App\Models\Event;
+
 return new class extends Migration
 {
     /**
@@ -14,6 +16,7 @@ return new class extends Migration
         Schema::create('kanbans', function (Blueprint $table) {
             $table->id();
             $table->string('name');
+            $table->foreignIdFor(Event::class);
             $table->timestamps();
             $table->softDeletes();
         });
