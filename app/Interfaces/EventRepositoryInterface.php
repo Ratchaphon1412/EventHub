b8e117618a497370c8ad7e844005bc4f814acea1
Tirawat1 <Tirawat.po@ku.ac.th>
2023-08-10 02:42:48 +0700

    update InterfacesEvent

--- app/Interfaces/EventRepositoryInterface.php
+++ app/Interfaces/EventRepositoryInterface.php
@@ -2,11 +2,13 @@
 
 namespace App\Interfaces;
 
+use Illuminate\Support\Facades\Auth;
+
 interface EventRepositoryInterface{
     public function getAllEvent();
     public function getEventByCategory($category);
     public function getEventByUserOwner($user);
-    public function createEvent($title,$description,$category,$image_poster,$date_post,$registration_start_date,$registration_end_date,$announcement_date,$event_start_date,$event_end_date,$event_latitude,$event_longitude,$document_payment,$user);
+    public function createEvent($title,$description,$category,$image_poster,$registration_start_date,$registration_end_date,$announcement_date,$event_start_date,$event_end_date,$event_latitude,$event_longitude,$document_payment,$user);
     public function findById($event_id);
     public function editEvent();
     public function deleteEvent();
