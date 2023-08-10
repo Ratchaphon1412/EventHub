d12cd128f072dd9d6ee4e9ce5b692b84eb77096b
Tirawat1 <Tirawat.po@ku.ac.th>
2023-08-10 16:50:47 +0700

    update passing in eventDetail

--- resources/views/eventDetail.blade.php
+++ resources/views/eventDetail.blade.php
@@ -23,7 +23,7 @@
                             <div class="flex flex-row">
                                 <i class="bi bi-geo-alt-fill mr-2"></i>
                                 <p>
-                                    Bitec Bang Na
+                                    Bitec Bang Na // Location
                                 </p>
                             </div>
                             <button type="button" class="py-2.5 px-5 mr-2 mb-2 text-sm font-medium  focus:outline-none text-white rounded-lg border border-gray-200 hover:bg-gray-100 hover:text-blue-700 focus:z-10 focus:ring-4 focus:ring-gray-200 dark:focus:ring-gray-700 dark:bg-gray-800 dark:text-gray-400 dark:border-gray-600 dark:hover:text-white dark:hover:bg-gray-700">Join</button>
@@ -53,33 +53,31 @@
             </div>
     
             <section id="Title" class="w-full text-center py-10 space-y-4">
-                <h1 class="mt-5 text-2xl md:text-4xl lg:text-5xl">Beautiful Mountain View</h1>
+                <h1 class="mt-5 text-2xl md:text-4xl lg:text-5xl">{{$event->title}}</h1>
                 <div class="flex flex-row justify-center">
                     <i class="bi bi-calendar-week font-bold mr-2"></i>
                     <p class=" leading-tight  text-base ">
-                        
-                    29 Sep 2023 17:00 - 30 Sep 2023 17:00
+                    {{date('d-m-Y',strtotime($event->registration_start_date))}} - {{date('d-m-Y',strtotime($event->registration_end_date))}}
                     </p>
                 </div>
                 <div class="flex flex-row  justify-center">
                     <i class="bi bi-geo-alt-fill mr-2"></i>
                     <p>
-                        Bitec Bang Na
+                        Bitec Bang Na // Location
                     </p>
                 </div>
                
                 <hr class="mt-5">
             </section>
             <section id="Image" class="flex flex-col justify-center items-center  first-letter w-full">
-                <img src="https://images.unsplash.com/photo-1501560379-05951a742668?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1470&q=80"  alt="Mountain" 
+                <img src="{{url('storage/'.$event->image_poster)}}"  alt="Mountain" 
                     class="w-3/4 h-full object-cover">
 
             </section>
             <section id="Detail">
                 <h1 class="mt-5 mx-20 text-2xl md:text-4xl lg:text-5xl">Detail</h1>
                 <p class="mt-5 mx-20 text-sm md:text-base lg:text-lg xl:text-xl">
-                    Join us for an unforgettable evening of musical brilliance as we present "A Night of Musical Magic" - a concert extravaganza that promises to leave you spellbound!
-                    The stage is set, the lights are dimmed, and the anticipation in the air is palpable. As the audience fills the grand concert hall, whispers of excitement mingle with the gentle hum of anticipation. Tonight is no ordinary night; it is a celebration of artistry and a union of soul-stirring melodies.
+                    {{$event->description}}
                 </p>
             </section>
 
@@ -99,8 +97,12 @@
             <section id="Contact" class="flex flex-col justify-center w-full space-y-4">
                 <h1 class="mt-5 mx-20 text-2xl md:text-4xl lg:text-5xl">Contact</h1>
                 <p class="mt-5 mx-20 text-sm md:text-base lg:text-lg xl:text-xl">
-                    Join us for an unforgettable evening of musical brilliance as we present "A Night of Musical Magic" - a concert extravaganza that promises to leave you spellbound!
-                    The stage is set, the lights are dimmed, and the anticipation in the air is palpable. As the audience fills the grand concert hall, whispers of excitement mingle with the gentle hum of anticipation. Tonight is no ordinary night; it is a celebration of artistry and a union of soul-stirring melodies.
+                    {{$event->user->name}} <br>
+                    {{$event->user->email}} <br>
+                    {{$event->user->phone}} <br>
+                    {{$event->user->facebook}} <br>
+                    {{$event->user->instagram}} <br>
+                    {{$event->user->line}} <br>
                 </p>
             </section>
 
