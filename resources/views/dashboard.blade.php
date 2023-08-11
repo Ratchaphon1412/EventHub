b2b8f6f11a704e92c8d85ef6929b9eb210899c02
Tirawat1 <Tirawat.po@ku.ac.th>
2023-08-11 23:04:46 +0700

    link to createEventPage from Dashboard

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -19,12 +19,15 @@
 
   <div class="grid md:grid-cols-3 sm:grid-cols-1 justify-center gap-4 ">
     <div class="group bg-gray-900/30 py-20 px-4 flex flex-col space-y-2 items-center  justify-center cursor-pointer rounded-md hover:bg-gray-900/40 hover:smooth-hover">
-      <a class="bg-gray-900/70 text-white/50 group-hover:text-white group-hover:smooth-hover flex w-20 h-20 rounded-full items-center justify-center" href="#">
+      <a href="{{route('event.create.view')}}" class="bg-gray-900/70 text-white/50 group-hover:text-white group-hover:smooth-hover flex w-20 h-20 rounded-full items-center justify-center" href="#">
         <svg xmlns="http://www.w3.org/2000/svg" class="h-10 w-10" fill="none" viewBox="0 0 24 24" stroke="currentColor">
           <path stroke-linecap="round" stroke-linejoin="round" stroke-width="1" d="M12 6v6m0 0v6m0-6h6m-6 0H6" />
         </svg>
       </a>
-      <a class="text-white/50 group-hover:text-white group-hover:smooth-hover text-center" href="#">Create group</a>
+      <a  href="{{route('event.create.view')}}"
+      class="text-white/50 group-hover:text-white group-hover:smooth-hover text-center">
+        Create event
+      </a>
     </div>
     @foreach ($user->eventOwner as $event)
     <a href="{{route('event.detail.show' , ['event'=>$event])}}">
