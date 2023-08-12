bcc4e4fc42559cd2ca9a6e56241f9b40b3c6713e
FolkXa <jirakit.chan@ku.th>
2023-08-13 04:36:44 +0700

    update route

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,8 @@
 <?php
 
 use App\Http\Controllers\AnswerQuestionController;
+use App\Http\Controllers\ApplicantAnswerController;
+use App\Http\Controllers\ApproveRegisterController;
 use App\Http\Controllers\CreateQuestionController;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\RegisterStepTwoController;
@@ -24,18 +26,18 @@ Route::middleware(['auth:sanctum',config('jetstream.auth_session'),'verified'])-
 
 
     Route::group(['middleware'=>['registration_completed']],function(){
-       Route::controller(DashboardController::class)->group(function(){
-              Route::get('/dashboard','index')->name('dashboard');
-       });
+        Route::controller(DashboardController::class)->group(function(){
+            Route::get('/dashboard','index')->name('dashboard');
+        });
 
-       Route::controller(KanbanController::class)->group(function (){
-        Route::get('/kanban/{id}','index')->name('kanban.index');
-        Route::post('/kanban','store')->name('kanban.store');
-        Route::post('/kanban/card/delete','destroy')->name('kanban.card.delete');
-        Route::post('/kanban/card/edit','edit')->name('kanban.card.edit');
-        Route::post('/kanban/card/update','update')->name('kanban.card.update');
+        Route::controller(KanbanController::class)->group(function (){
+            Route::get('/kanban/{id}','index')->name('kanban.index');
+            Route::post('/kanban','store')->name('kanban.store');
+            Route::post('/kanban/card/delete','destroy')->name('kanban.card.delete');
+            Route::post('/kanban/card/edit','edit')->name('kanban.card.edit');
+            Route::post('/kanban/card/update','update')->name('kanban.card.update');
 
-    });
+        });
 
     });
 
@@ -44,6 +46,16 @@ Route::middleware(['auth:sanctum',config('jetstream.auth_session'),'verified'])-
     Route::get('register-step-two',[RegisterStepTwoController::class,'create'])->name('register-step-two.view');
     Route::post('register-step-two',[RegisterStepTwoController::class,'store'])->name('register-step-two.store');
 });
+
+Route::controller(ApproveRegisterController::class)->group(function (){
+    Route::get('/{event}/approve_register', 'index')->name('approve.register');
+    Route::post('/{event}/approve_register', 'update')->name('approve.update');
+});
+
+
+Route::get('/{event}/approve_register/{applicant}', [
+    ApplicantAnswerController::class, 'index'
+])->name('applicant.answer');
 
 Route::controller(CreateQuestionController::class)->group(function (){
     Route::get('/{event}/create_question', 'index')->name('question.create');
@@ -54,10 +66,6 @@ Route::controller(AnswerQuestionController::class)->group(function (){
     Route::get('/{event}/answer_question', 'index')->name('question.answer');
     Route::post('/{event}/answer_question', 'store')->name('question.answer.store');
 });
-
-Route::get('/test', [
-    \App\Http\Controllers\TestController::class
-])->name('test');
 
 
 Route::get('/detail',function (){
