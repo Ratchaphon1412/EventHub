2e3906f2bedf930f1e0b774a0fe4258a8a10e102
Tirawat1 <Tirawat.po@ku.ac.th>
2023-08-18 23:40:42 +0700

    add team

--- resources/views/editEvent.blade.php
+++ resources/views/editEvent.blade.php
@@ -254,7 +254,6 @@
 
         
             <button type="submit" class="block w-full bg-blue-500 text-white font-bold p-4 rounded-lg">Update Form</button>
-
         </form>
         <form action="{{route('event.delete',['event' => $event])}}" enctype="multipart/form-data" method="POST" class="flex flex-col justify-start container lg:px-24 space-x-2 space-y-4">
             @csrf
