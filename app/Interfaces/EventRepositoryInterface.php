bcee75f1be51496a98060e0087ed3a1e1a2d6a5e
Tirawat1 <Tirawat.po@ku.ac.th>
2023-08-19 23:49:55 +0700

    certificate add to table

--- app/Interfaces/EventRepositoryInterface.php
+++ app/Interfaces/EventRepositoryInterface.php
@@ -9,7 +9,7 @@ interface EventRepositoryInterface
     public function getAllEvent();
     public function getEventByCategory($category);
     public function getEventByUserOwner($user);
-    public function createEvent($title, $description, $category, $image_poster, $registration_start_date, $registration_end_date, $announcement_date, $event_start_date, $event_end_date, $event_latitude, $event_longitude, $document_payment, $user, $location_detail, $contact, $location_name);
+    public function createEvent($title, $description, $category, $image_poster, $registration_start_date, $registration_end_date, $announcement_date, $event_start_date, $event_end_date, $event_latitude, $event_longitude, $document_payment,$certificate_file, $user, $location_detail, $contact, $location_name);
     public function findById($event_id);
     public function editEvent($event);
     public function deleteEvent($event);
