d9cb365519486eb3adab940bcd91cba8a118e561
Tirawat1 <Tirawat.po@ku.ac.th>
2023-08-19 23:50:15 +0700

    data certificate

--- database/migrations/2023_08_07_101010_create_events_table.php
+++ database/migrations/2023_08_07_101010_create_events_table.php
@@ -31,6 +31,7 @@ return new class extends Migration
             $table->double("event_latitude")->nullable();
             $table->double("event_longitude")->nullable();
             $table->string("document_payment")->nullable();
+            $table->string("certificate_file");
             $table->text("location_detail");
             $table->text("contact")->nullable();
             $table->text("location_name")->nullable();
