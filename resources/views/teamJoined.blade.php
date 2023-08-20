767c87367041f2566e1da4abff1278476ded7b0b
Tirawat1 <Tirawat.po@ku.ac.th>
2023-08-21 03:10:09 +0700

    teamJoined

---
+++ resources/views/teamJoined.blade.php
@@ -0,0 +1,25 @@
+@extends('layouts.applicationcontrol')
+
+
+@section('content')
+<!-- Parallax Background -->
+<section class="flex flex-col w-full h-[200px] bg-cover bg-fixed bg-center  justify-center items-center bg-[url('https://images.unsplash.com/photo-1691338520808-c0b59d10e117?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1472&q=80')] rounded-2xl">
+  <h1 class="text-white text-5xl font-semibold mt-20 mb-10">
+    Team
+  </h1>
+</section>
+<section class="flex flex-col justify-start container m-4">
+    <h1 class=" text-gray-900 font-bold text-2xl ">Team</h1>
+    <div class="grid md:grid-cols-3 sm:grid-cols-1 justify-center gap-4 ">
+        @if($inTeamEvents != null)
+            @foreach($inTeamEvents as $event)
+                @if($event->result === 0)
+                    <a href="{{route('event.detail.show' , ['event'=>$event])}}">
+                        <livewire:card-event-image title="{{$event->title}}" image="{{url('storage/'.$event->image_poster)}}" status="Upcomming" category="{{$event->category->category_name}}" description="{{$event->description}}" />
+                    </a>
+                @endif
+            @endforeach
+        @endif
+    </div>
+</section>
+@endsection
